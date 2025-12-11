0139d4a78632c88363b76f2f053e9e11dbae5f3a
Joel <joel_van_houten@hotmail.com>
2025-12-11 11:24:52 +0100

    Base - componentlibrary/bootstrap/pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router.ts";
+
 import "./styles/main.css";
 import "./styles/variables.css";
 import "./styles/animations.css";
-import '@fortawesome/fontawesome-free/css/all.css'
+import "@fortawesome/fontawesome-free/css/all.css";
 import "@fontsource-variable/roboto-mono";
 import "@fontsource-variable/montserrat";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+App.use(createPinia());
 
 createApp(App).use(router).mount("#app");
