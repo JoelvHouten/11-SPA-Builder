054e8540e58f2e1bcdaca729080aa3787111e66c
Fimtendo <62545607+Fimtendo@users.noreply.github.com>
2025-12-18 09:59:51 +0100

    created a fairly basic lesson page based on the mockup

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import ComponentGallery from "./views/ComponentGallery.vue";
+import LessonComponent from "./views/LessonComponent.vue";
 
-const routes = [{ path: "/", component: ComponentGallery }];
+const routes = [{ path: "/", component: ComponentGallery }, 
+				{ path: "/lesson", component: LessonComponent },];
 
 export const router = createRouter({
 	history: createMemoryHistory(),
