b41a9b432de4a6fe6cda4a8cdb4aed8cc448a5dc
Jelmer van der Meer <jelmervandermeer02@gmail.com>
2026-01-08 09:17:13 +0100

    README datastructuur Pinia store en installatie van de builder

--- src/paginas/TestStore.vue
+++ src/paginas/TestStore.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useLessenStore } from '../stores/lessenStore'
+import { useLessenStore } from '../stores/lessonStore'
 import { onMounted } from 'vue'
 import BasicButton from '../components/Basic/BasicButton.vue'
 
