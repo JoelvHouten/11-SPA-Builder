f3258f2cb398c346d295e3594ac7c8c8e64c9da2
Joel <158784536+JoelvHouten@users.noreply.github.com>
2026-01-14 19:44:47 +0100

    Revert "Feature/home lessoncards"

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import "@fontsource-variable/roboto-mono";
 import "@fontsource-variable/montserrat";
 
+
 const app = createApp(App);
 const pinia = createPinia();
 
