04923fcba9d01068d0d7f6c5792fd96e28e271af
Jelmer van der Meer <jelmervandermeer02@gmail.com>
2026-01-14 15:15:53 +0100

    LessonOverview aangemaakt Router aangepast zodat deze pagina zichtbaar is

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,10 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import ComponentGallery from "./views/ComponentGallery.vue";
 import LessonComponent from "./views/LessonComponent.vue";
+import LessonOverview from "./views/LessonOverview.vue";
 
-const routes = [{ path: "/", component: ComponentGallery }, 
+const routes = [{ path: "/", component: ComponentGallery },
+	            { path: "/lessons", component: LessonOverview }, 
 				{ path: "/lesson", component: LessonComponent },];
 
 export const router = createRouter({
