e1bd9b14f4250bb319d87b6b925bd4998a472018
Nick Riemersma <pieterstienstra@hotmail.com>
2026-01-14 13:41:08 +0100

    MERG GING ERGENS FOUT BRBR

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,9 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import ComponentGallery from "./views/ComponentGallery.vue";
 import LessonComponent from "./views/LessonComponent.vue";
+import LessonDemo from "./views/LessonDemo.vue";
 
-const routes = [{ path: "/", component: ComponentGallery }, 
+const routes = [{ path: "/", component: LessonDemo },
 				{ path: "/lesson", component: LessonComponent },];
 
 export const router = createRouter({
--- src/views/LessonComponent.vue
+++ src/views/LessonComponent.vue
@@ -2,6 +2,23 @@
 import { ref, computed } from "vue";
 import BasicButton from "../components/Basic/BasicButton.vue";
 import Hero from "../components/blocks/headimage.vue";
+import DraggableGrid from "@/components/DraggableGrid/DraggableGrid.vue";
+import BasicCard from "@/components/Basic/BasicCard.vue";
+
+interface Card {
+  title: string
+  innerText: string
+  img?: string
+}
+
+const items = ref<Card[]>([
+  { title: 'Les titel', innerText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis, quam eu sagittis consequat, nisl eros feugiat sapien, quis congue tellus urna ut sem.'},
+  { title: 'JEP', innerText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis, quam eu sagittis consequat, nisl eros feugiat sapien, quis congue tellus urna ut sem.'},
+  {
+    title: 'Ja nog een',
+    innerText: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don\'t care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let\'s shake it up a little. Barry! Breakfast is ready! Coming! Hang on a second. Hello? - Barry? - Adam? - Can you believe this is happening? - I can\'t. I\'ll pick you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry. I\'m excited. Here\'s the graduate. We\'re very proud of you, son. A perfect report card, all B\'s. Very proud. Ma! I got a thing going here. - You got lint on your fuzz. - Ow! That\'s me! - Wave to us! We\'ll be in row 118,000. - Bye! Barry, I told you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A little. Special day, graduation. Never thought I\'d make it. Three days grade school, three days high school. Those were awkward. Three days college. I\'m glad I took a day and hitchhiked around the hive. You did come back different. - Hi, Barry. - Artie, growing a mustache? Looks good. - Hear about Frankie? - Yeah. - You going to the funeral? - No, I\'m not going. Everybody knows, sting someone, you die. Don\'t waste it on a squirrel. Such a hothead. I guess he could have just gotten out of the way. I love this incorporating an amusement park into our day. That\'s why we don\'t need vacations. Boy, quite a bit of pomp... under the circumstances. - Well, Adam, today we are men. - We are! - Bee-men. - Amen! Hallelujah!'
+  }
+])
 
 const props = defineProps({
   title: { type: String, default: "Lesson Title" },
@@ -63,22 +80,33 @@ function selectAnswer(idx: number) {
         <p class="lesson__task-text">{{ taskText }}</p>
       </div>
 
-      <div class="lesson__block lesson__block--question">
-        <h2 class="lesson__question-title">Question</h2>
-        <p class="lesson__question-body">{{ question }}</p>
-        <div class="lesson__answers" role="list">
-          <BasicButton
-            v-for="(label, idx) in labels"
-            :key="idx"
-            :label="label"
-            class="lesson__answer"
-            :class="{ 'lesson__answer--selected': selected === idx }"
-            @click="selectAnswer(idx)"
-            role="listitem"
-            :aria-pressed="selected === idx"
-          />
-        </div>
+      <div class="page__wrapper">
+        <DraggableGrid v-model="items" class="page__grid">
+          <template #default="{ item }">
+            <BasicCard
+              :title="item.title"
+              :innerText="item.innerText"
+              :img="item.img"
+            />
+          </template>
+        </DraggableGrid>
       </div>
+<!--      <div class="lesson__block lesson__block&#45;&#45;question">-->
+<!--        <h2 class="lesson__question-title">Question</h2>-->
+<!--        <p class="lesson__question-body">{{ question }}</p>-->
+<!--        <div class="lesson__answers" role="list">-->
+<!--          <BasicButton-->
+<!--            v-for="(label, idx) in labels"-->
+<!--            :key="idx"-->
+<!--            :label="label"-->
+<!--            class="lesson__answer"-->
+<!--            :class="{ 'lesson__answer&#45;&#45;selected': selected === idx }"-->
+<!--            @click="selectAnswer(idx)"-->
+<!--            role="listitem"-->
+<!--            :aria-pressed="selected === idx"-->
+<!--          />-->
+<!--        </div>-->
+<!--      </div>-->
     </section>
   </main>
 </template>
@@ -90,6 +118,25 @@ $muted: #626262;
 $border: #e6e6e6;
 $accent: #79c4fa;
 $text-color: #fdfdfd;
+
+.page__title {
+  margin: 0;
+  font-size: 2.25rem;
+  font-weight: 600;
+}
+
+.page__wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.page__grid {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100%;
+  max-width: 50rem;
+}
 
 .lesson {
   display: block;
@@ -161,9 +208,9 @@ $text-color: #fdfdfd;
     border-radius: 8px;
 
     &--lesson {
-      .lesson__text { 
-            color: $text-color; 
-            margin: 0; 
+      .lesson__text {
+            color: $text-color;
+            margin: 0;
         }
     }
 
@@ -179,14 +226,14 @@ $text-color: #fdfdfd;
     }
 
     &--question {
-      
+
         .lesson__question-title {
             margin: 0 0 8px 0;
-            font-size: 1rem; 
+            font-size: 1rem;
         }
         .lesson__question-body {
             margin: 0 0 12px 0;
-            color: $text-color; 
+            color: $text-color;
         }
 
         .lesson__answers {
